516752d20ea317d5a08d8b8884eb86c49e590a3c
Thomas Borde <thomas.borde94@hotmail.fr>
2023-03-20 08:37:17 -0700

    added import storage in state.py

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
+from models import storage
 
 
 class State(BaseModel, Base):
