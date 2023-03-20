dbac4c428ab91aa81126c8512abf0e2c4182ee7c
ali segueg <alisegueg123@outllok.fr>
2023-03-20 14:50:03 +0100

    mise a jour de fonction

--- models/city.py
+++ models/city.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
 class City(BaseModel, Base):
@@ -8,3 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship('Place', cascade='all, delete', backref='cities')
--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 class User(BaseModel):
     """indiquer le nom de la table dans laquelle les
@@ -11,3 +13,11 @@ class User(BaseModel):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    """
+    cette ligne de commande crée une relation entre l'utilisateur et les
+    places qu'il possède. Si un utilisateur est supprimé, toutes les places
+    qui lui sont associées seront également supprimées en cascade
+    backref='user'permet accéder facilement à l'utilisateur associé à une place.
+    """
+    places = relationship('Place', cascade='all, delete', backref='user')
