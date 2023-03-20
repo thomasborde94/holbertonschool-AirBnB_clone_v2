41e475fc0699c808820f37b2d6d2bb3cd91eccce
Thomas Borde <thomas.borde94@hotmail.fr>
2023-03-20 08:21:05 -0700

    pushing state.py for debugging purposes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    @property
+    def cities(self):
+        """returns instances of City"""
+        cityList = []
+        cities = storage.all(City)
+        for city in cities.values():
+            if self.id == city.state_id:
+                cityList.append(city)
+        return cityList
