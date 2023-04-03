d421013b5aba9b58109cea28204d090af56e828b
Thomas Borde <thomas.borde94@hotmail.fr>
2023-04-03 05:25:42 -0700

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Displays Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """Displays C followed with value of text"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def text(text="is cool"):
+    """Displays C followed with value of text"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
